44c98d3d0692da865cf7998177cf77b9123b49cd
Stephen Brown <stephen.jr36@gmail.com>
2024-08-14 12:45:01 -0600

    created session functionality with login and logout functionality

--- app.js
+++ app.js
@@ -6,6 +6,20 @@ const PORT = 8888;
 
 const app = express();
 
+// Allows express to decode a body object???
+app.use(express.urlencoded({
+    extended: false
+}))
+
+// Allows express to set up a session
+app.use(
+    session({
+      secret: "ssshhhhh",
+      saveUninitialized: true,
+      resave: false,
+    })
+  );
+
 // Configure nunjucks
 nunjucks.configure("views", {
     autoescape: true,
@@ -20,3 +34,32 @@ app.get("/", (req, res) => {
     res.render("index.html")
 })
 
+// Login GET route
+app.get("/login", (req, res) => {
+    // if req.session.username exists, then this client has previously "logged in"
+    if (req.session.username) {
+        res.render("dashboard.html", {
+            username: req.session.username,
+        })
+    } else {
+        res.render("login.html")
+    }
+})
+
+// Login POST route
+app.post("/login", (req, res) => {
+    req.session.username = req.body.username;
+    
+    res.render("dashboard.html", {
+        username: req.session.username,
+    })
+})
+
+// Logout route
+app.get("/logout", (req, res) => {
+    // "Destoy" the session, and navigate back home
+    req.session.destroy();
+    
+    // Redirects to Home route
+    res.redirect("/")
+})
